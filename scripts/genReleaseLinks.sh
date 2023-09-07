ef30382fee24f4ae7d481ac607585f6d2c7d8a30
frc4533-lincoln <132951735+frc4533-lincoln@users.noreply.github.com>
2023-09-07 16:27:44 +0000

    Update genReleaseLinks.sh

--- scripts/genReleaseLinks.sh
+++ scripts/genReleaseLinks.sh
@@ -2,7 +2,7 @@
 
 ver='2023.4.3'
 
-echo -e " > **${ver}**\n \
+printf " > **${ver}**\n \
  >  - [Linux (x86_64)](https://github.com/wpilibsuite/allwpilib/releases/download/v${ver}/WPILib_Linux-${ver}.tar.gz)\n \
  >  - [Windows (x86_64)](https://github.com/wpilibsuite/allwpilib/releases/download/v${ver}/WPILib_Windows-${ver}.iso)\n \
  >  - [Mac (Apple Silicon)](https://github.com/wpilibsuite/allwpilib/releases/download/v${ver}/WPILib_macOS-Intel-${ver}.dmg)\n \
